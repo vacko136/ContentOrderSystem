4cde6eef946e6d9be649bf0a3099847f674bd1c9
David <davod.vacko@gmail.com>
2025-10-26 10:33:46 +0100

    Add repository stubs for Articles and Orders (pseudocode EF)

---
+++ Infrastructure/Repositories/EfArticleRepository.cs
@@ -0,0 +1,27 @@
+﻿// Infrastructure/Repositories/EfArticleRepository.cs
+using System;
+using System.Collections.Generic;
+using ContentOrderSystem.Domain;
+using ContentOrderSystem.Application.Interfaces;
+
+namespace ContentOrderSystem.Infrastructure.Repositories
+{
+    public class EfArticleRepository : IRepository<Article>
+    {
+        private readonly List<Article> _articles = new(); // Simulated DB
+
+        public void Add(Article entity) => _articles.Add(entity);
+
+        public void Delete(Article entity) => _articles.Remove(entity);
+
+        public IEnumerable<Article> GetAll() => _articles;
+
+        public Article GetById(Guid id) => _articles.Find(a => a.Id == id);
+
+        public void Update(Article entity)
+        {
+            var index = _articles.FindIndex(a => a.Id == entity.Id);
+            if (index >= 0) _articles[index] = entity;
+        }
+    }
+}
---
+++ Infrastructure/Repositories/EfOrderRepository.cs
@@ -0,0 +1,28 @@
+﻿// Infrastructure/Repositories/EfOrderRepository.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ContentOrderSystem.Domain;
+using ContentOrderSystem.Application.Interfaces;
+
+namespace ContentOrderSystem.Infrastructure.Repositories
+{
+    public class EfOrderRepository : IRepository<Order>
+    {
+        private readonly List<Order> _orders = new(); // Simulated DB
+
+        public void Add(Order entity) => _orders.Add(entity);
+
+        public void Delete(Order entity) => _orders.Remove(entity);
+
+        public IEnumerable<Order> GetAll() => _orders;
+
+        public Order GetById(Guid id) => _orders.Find(o => o.Id == id);
+
+        public void Update(Order entity)
+        {
+            var index = _orders.FindIndex(o => o.Id == entity.Id);
+            if (index >= 0) _orders[index] = entity;
+        }
+    }
+}
