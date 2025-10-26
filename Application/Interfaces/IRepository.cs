93a8883fa159b2e24bf9bb102ee8b73eb7aa28ed
David <davod.vacko@gmail.com>
2025-10-26 10:24:11 +0100

    Add Application interfaces: IRepository, IUnitOfWork, ISmsGateway

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿// Application/Interfaces/IRepository.cs
+using System;
+using System.Collections.Generic;
+
+namespace ContentOrderSystem.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(Guid id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿// Application/Interfaces/IUnitOfWork.cs
+using ContentOrderSystem.Domain;
+using System;
+
+namespace ContentOrderSystem.Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Article> Articles { get; }
+        IRepository<SubscriptionPackage> SubscriptionPackages { get; }
+        IRepository<Order> Orders { get; }
+        void Commit();
+    }
+}
